b1bbbb919b843b1bcb63e0499687b8950b27c319
student76 <arbgalla@ucsc.edu>
2015-12-10 12:11:04 -0800

    bugs bugs bugs

--- models/menu.py
+++ models/menu.py
@@ -53,6 +53,7 @@ def user_bar():
         head = LI(text_head,_class='dropdown-header')
         lang = A(text_language, _href='#', _id='lang')
         logout = A(text_logout, _href=action+'/logout')
+        admin=None
         if (auth.has_membership(1,auth.user_id)):
             admin = A(text_admin,_href=URL('default','admin'))
         profile = A(text_profile, _href=action+'/profile')
